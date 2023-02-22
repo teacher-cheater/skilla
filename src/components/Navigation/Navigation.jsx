9565fc7dd351cd70c1ddfbc7ef3ea99379c64b35
teacher_cheater <johnnykayfun@gmail.com>
2023-02-23 01:02:15 +0300

    add aside

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import s from "./Navigation.module.css";
+import logo from "../../icons/logo.svg";
+import btnAdd from "../../icons/btn-add.svg";
+import btnPay from "../../icons/btn-pay.svg";
+import calls from "./iconsList/calls.svg";
+import cases from "./iconsList/case.svg";
+import documents from "./iconsList/documents.svg";
+import library from "./iconsList/library.svg";
+import mail from "./iconsList/mail.svg";
+import orders from "./iconsList/orders.svg";
+import people from "./iconsList/people.svg";
+import person from "./iconsList/person.svg";
+import settings from "./iconsList/settings.svg";
+import timeline from "./iconsList/timeline.svg";
+
+function Navigation() {
+  return (
+    <aside className={s.wrapper}>
+      <a href="#">
+        <img className={s.logo} src={logo} alt="logo" />
+      </a>
+      <nav className={s.navigtion}>
+        <ul className={s.list}>
+          <li>
+            {" "}
+            <img className={s.icon} src={timeline} alt="" /> Итоги
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={orders} alt="" /> Заказы
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={mail} alt="" /> Сообщения
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={calls} alt="" /> Звонки
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={people} alt="" /> Контрагенты
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={documents} alt="" /> Документы
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={person} alt="" /> Исполнители
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={cases} alt="" /> Отчеты
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={library} alt="" /> База знаний
+          </li>
+          <li>
+            {" "}
+            <img className={s.icon} src={settings} alt="" /> Настройки
+          </li>
+        </ul>
+      </nav>
+      <button className={s.btn}>
+        Добавить заказ <img src={btnAdd} alt="icons" />{" "}
+      </button>
+      <button className={s.btn}>
+        Оплата <img src={btnPay} alt="icons" />{" "}
+      </button>
+    </aside>
+  );
+}
+
+export default Navigation;
